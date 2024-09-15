d2a26fa2f63473a9b685485fa30fcf74676e9022
girivardhan1919 <daggupati.girivardhan@gmail.com>
2024-09-16 02:17:32 +0530

    Added Login Service APIs, rate limiter and proxy compliant

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,52 @@
+const {StatusCodes} = require('http-status-codes');
+
+const {UserService} = require('../services');
+const {ErrorResponse, SuccessResponse} = require('../utils/common');
+const { create } = require('../services/user-service');
+
+/**
+ * POST : /signup 
+ * req-body {email: 'a@b.com', password: '1234'}
+ */
+async function signup(req, res) {
+  try {
+    const user = await UserService.create({
+      email: req.body.email,
+      password: req.body.password
+    });
+    SuccessResponse.data = user;
+    return res
+            .status(StatusCodes.CREATED)
+            .json(SuccessResponse);
+  } catch (error) {
+    console.log(error);
+    ErrorResponse.error = error;
+    return res
+            .status(error.statusCode)
+            .json(ErrorResponse);
+  }
+}
+
+async function signin(req, res) {
+  try {
+    const user = await UserService.signin({
+      email: req.body.email,
+      password: req.body.password
+    });
+    SuccessResponse.data = user;
+    return res
+            .status(StatusCodes.CREATED)
+            .json(SuccessResponse);
+  } catch (error) {
+    console.log(error);
+    ErrorResponse.error = error;
+    return res
+            .status(error.statusCode)
+            .json(ErrorResponse);
+  }
+}
+
+module.exports = {
+  signup,
+  signin
+}
